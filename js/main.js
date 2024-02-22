807dbcca2e82d5a478824697e85824925bfa51e9
Joseph Ehigiator <joeehis1@gmail.com>
2024-02-22 00:27:23 -0500

    Changed sound file link to a url

--- js/main.js
+++ js/main.js
@@ -142,9 +142,11 @@ async function setAnswered(id, option) {
 async function playSound(answeredCorrectly) {
     const audio = new Audio();
     if (answeredCorrectly) {
-        audio.src = "../assets/correct.wav";
+        audio.src =
+            "https://github.com/joeehis1/coding-quiz/raw/main/assets/correct.wav";
     } else {
-        audio.src = "../assets/incorrect.wav";
+        audio.src =
+            "https://github.com/joeehis1/coding-quiz/raw/main/assets/correct.wav";
     }
     return audio.play();
 }
